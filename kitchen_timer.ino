b35ff9a078deb076be28f3d823f29d19f8879422
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-05-08 10:01:03 +0200

    Update kitchen_timer.ino

--- kitchen_timer.ino
+++ kitchen_timer.ino
@@ -1,46 +1,34 @@
+// kitchen timer
+// only TM1637 7segment  rotary encoder and buzzer (NOT with 2x16 LCD)
+// version 8 - may - 2021
+// https://www.youtube.com/watch?v=B5O9bT54BzI
+// TM1637 7segment https://www.youtube.com/watch?v=a7cO0Zcwmvw
+// original with seperate start button https://www.youtube.com/watch?v=rzxisoU9D6c
+
+// TM1637 4 digit 7segment
+// rotary encoder
+// buzzer
 
 #include <Arduino.h>
 #include <TM1637Display.h> //https://github.com/avishorp/TM1637
-
+#include <Wire.h>
 
 // Module connection pins (Digital Pins)
 #define CLK 8           // D8 to  TM1637 4 character 7 segment LED Display
 #define DIO 9            // D9 to  TM1637 4 character 7 segment LED Display
 
-// i2c 16x2 LCD   to  SCL SDA or A4 A5
-
-
-
-
 // The amount of time (in milliseconds) between tests
 #define TEST_DELAY   2000
-
 
 const uint8_t SEG_DONE[] = {
   SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
   SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
   SEG_C | SEG_E | SEG_G,                           // n
   SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
-  };
+};
 
 
 TM1637Display display(CLK, DIO);
-
-
-
-
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-//#include <LCD.h>
-
-//version 29-2-2020
-//https://www.youtube.com/watch?v=B5O9bT54BzI
-// lcd and TM1637 7segmnent https://www.youtube.com/watch?v=a7cO0Zcwmvw
-//original with seperate start button https://www.youtube.com/watch?v=rzxisoU9D6c
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-//LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
-//LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address
-//LiquidCrystal_I2C lcd(0x38);  // Set the LCD I2C address
 
 
 #define Start 4               // start stop button D4
@@ -77,16 +65,10 @@ void setup() {
   pinMode(encoderButton, INPUT_PULLUP);
   attachInterrupt(0, doEncoderA, CHANGE); //pin 2
   attachInterrupt(1, doEncoderB, CHANGE); //pin 3
-  Serial.begin(9600); // output
-  lcd.begin(); // initialize the lcd for 16 chars 2 lines, turn on backlight
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("COUNT DOWN TIMER");
-  lcd.setCursor(4, 1);
-  lcd.print("00:00:00");
-  delay(2500);
 
-HMS = 1;
+  Serial.begin(9600); // output
+
+  HMS = 1;
 }
 
 
@@ -102,37 +84,37 @@ void loop() {
     }
     tone(BuzzerPin, 1000, 100);
     int t = 80;
-    while (digitalRead(encoderButton) == LOW) {   
-      delay(1); t = t - 1; lcd.setCursor(0, 0); lcd.print(t); lcd.print("          ");
-      if (t == 60) {tone(BuzzerPin, 200, 50);}
-        if (t == 40) {tone(BuzzerPin, 200, 50);}
-          if (t == 20) {tone(BuzzerPin, 200, 50);}
+    while (digitalRead(encoderButton) == LOW) {
+      delay(11);
+      t = t - 1;
+      //lcd.setCursor(0, 0); lcd.print(t); lcd.print("          ");
+      if (t == 60) {
+        tone(BuzzerPin, 200, 50);
+      }
+      if (t == 40) {
+        tone(BuzzerPin, 200, 50);
+      }
+      if (t == 20) {
+        tone(BuzzerPin, 200, 50);
+      }
       if (t <= 1) {
         t = 1; timeState = true;
-            sethours = hours;
-            setminutes=minutes;
-            setseconds=seconds;
-        lcd.setCursor(0, 0); lcd.print("Get Set Go      ");
+        sethours = hours;
+        setminutes = minutes;
+        setseconds = seconds;
+        // lcd.setCursor(0, 0); lcd.print("Get Set Go      ");
         tone(BuzzerPin, 2000, 100);
       }
     }
   }
-  //  lcd.setCursor(0, 0);
-  //  lcd.print("COUNT DOWN TIMER");
-  //  delay(500);
-  if (HMS == 3) {
-    lcd.setCursor(0, 0);
-    lcd.print("set hours       ");
-  }
+
   if (HMS == 2) {
-    lcd.setCursor(0, 0);
-    lcd.print("set minutes     ");
+    Serial.println("set minutes     ");
   }
   if (HMS == 1) {
-    lcd.setCursor(0, 0);
-    lcd.print("set seconds     ");
+    Serial.println("set seconds     ");
   }
-  //delay(500);
+
 
 
   rotating = true; // reset the debouncer
@@ -143,17 +125,17 @@ void loop() {
     if (HMS == 3) {
       hours = hours + encoderPos;
       hours = constrain(hours, 0, 48);
-     
+
     }
     else if (HMS == 2) {
       minutes = minutes + encoderPos;
       minutes = constrain(minutes, 0, 99);
-     
+
     }
     else if (HMS == 1) {
       seconds = seconds + encoderPos;
       seconds = constrain(seconds, 0, 60);
-      
+
     }
 
 
@@ -173,58 +155,58 @@ void loop() {
 
 
 
-    lcd.setCursor(4, 1);
+    // lcd.setCursor(4, 1);
     if (hours <= 9)
     {
-      lcd.print("0");
+      //   lcd.print("0");
     }
-    lcd.print(hours);
-    lcd.print(":");
+    // lcd.print(hours);
+    //  lcd.print(":");
     if (minutes <= 9)
     {
-      lcd.print("0");
+      //   lcd.print("0");
     }
-    lcd.print(minutes);
-    lcd.print(":");
+    //  lcd.print(minutes);
+    //  lcd.print(":");
     if (seconds <= 9)
     {
-      lcd.print("0");
+      //  lcd.print("0");
     }
-    lcd.print(seconds);
+    // lcd.print(seconds);
     encoderPos = 0;
     lastReportedPos = encoderPos;
-    
-  
-  display.setBrightness(0x0f);
- if (minutes == 0)display.clear();
- if (minutes>0)display.showNumberDec(seconds, true, 2, 2);  // Expect:  1:04
- if (minutes==0)display.showNumberDec(seconds, false, 2, 2);  // Expect: no leading 0
- if (minutes > 0) display.showNumberDecEx(minutes, (0b01000000), false,2,0); 
-   
-   
+
+
+    display.setBrightness(0x0f);
+    if (minutes == 0)display.clear();
+    if (minutes > 0)display.showNumberDec(seconds, true, 2, 2); // Expect:  1:04
+    if (minutes == 0)display.showNumberDec(seconds, false, 2, 2); // Expect: no leading 0
+    if (minutes > 0) display.showNumberDecEx(minutes, (0b01000000), false, 2, 0);
+
+
   }
 
 
 
 
   if (digitalRead(Start) == LOW || timeState == true) { //start count down timer
-    lcd.setCursor(0, 0); lcd.print("Counting Down!  ");
+    //lcd.setCursor(0, 0); lcd.print("Counting Down!  ");
     timeState = true;
     delay(500);
-    
+
     while (timeState == true) {
-       
-    //delay(992);           //maintime delay
-    int period = 1000;
-    unsigned long time_now = 0;
-    time_now = millis();
-    while(millis() < time_now + period){ //wait approx. [period] ms}
+
+      //delay(992);           //maintime delay
+      int period = 1000;
+      unsigned long time_now = 0;
+      time_now = millis();
+      while (millis() < time_now + period) { //wait approx. [period] ms}
 
 
-      
-    }
-       seconds = seconds - 1;
-       
+
+      }
+      seconds = seconds - 1;
+
       if (minutes == 0 && hours >= 1) {
         minutes = 60;
         hours = hours - 1;
@@ -235,15 +217,15 @@ void loop() {
       }
       else if (minutes == 0 && hours == 0 && seconds == 0) { //count down alarm
         while (timeState == true) {
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("COUNT DOWN TIMER");
-            lcd.setCursor(5, 1);
-            lcd.print("Alarm!");
+          // lcd.clear();
+          //  lcd.setCursor(0, 0);
+          //  lcd.print("COUNT DOWN TIMER");
+          //  lcd.setCursor(5, 1);
+          //  lcd.print("Alarm!");
 
 
- display.setSegments(SEG_DONE);
-            
+          display.setSegments(SEG_DONE);
+
           tone(BuzzerPin, 600, 250);
           delay(250);
           tone(BuzzerPin, 800, 250);
@@ -251,15 +233,15 @@ void loop() {
           if (digitalRead(Start) == LOW) { // turn alarm off with switch
             timeState = false;
             seconds = 1;
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("COUNT DOWN TIMER");
-            lcd.setCursor(4, 1);
-            lcd.print("00:00:00");
-            
+            //  lcd.clear();
+            //  lcd.setCursor(0, 0);
+            //  lcd.print("COUNT DOWN TIMER");
+            //  lcd.setCursor(4, 1);
+            //  lcd.print("00:00:00");
+
             hours = sethours;
-            minutes=setminutes;
-            seconds=setseconds;
+            minutes = setminutes;
+            seconds = setseconds;
 
 
             break;
@@ -269,27 +251,24 @@ void loop() {
           if (digitalRead(encoderButton) == LOW) { // turn alarm off with rotary encoder switch
             timeState = false;
             //seconds = 1;
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("COUNT DOWN TIMER");
-            lcd.setCursor(4, 1);
-            lcd.print("Done!");
-            while(digitalRead(encoderButton) == LOW){delay(1000);}//wait for release switch
+            ///  lcd.clear();
+            //  lcd.setCursor(0, 0);
+            // lcd.print("COUNT DOWN TIMER");
+            //  lcd.setCursor(4, 1);
+            //   lcd.print("Done!");
+            while (digitalRead(encoderButton) == LOW) {
+              delay(1000); //wait for release switch
+            }
             hours = sethours;
-            minutes=setminutes;
-            seconds=setseconds;
+            minutes = setminutes;
+            seconds = setseconds;
             break;
           }
 
 
         }
       }
-    // delay(992);
-//if (minutes > 0){
-//if (seconds == 0)seconds=60;
-//}
 
-  //    seconds = seconds - 1;
 
 
       //Serial.print('hours '); // for testing
@@ -301,55 +280,55 @@ void loop() {
       //Serial.println(' ');
 
 
-  int k;
-  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
+      int k;
+      uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+      uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
 
 
- 
-  //display.clear();
-  //display.showNumberDecEx(14, (0b01000000), false,2,0);
+
+      //display.clear();
+      //display.showNumberDecEx(14, (0b01000000), false,2,0);
 
 
-  
-  //display.showNumberDec(14, true, 2, 0); // Expect: __14
-  //delay(TEST_DELAY);  
-  
-  display.setBrightness(0x0f);
-  if (minutes == 0)display.clear();
- if (minutes>0)display.showNumberDec(seconds, true, 2, 2);  // Expect:  1:04
- if (minutes==0)display.showNumberDec(seconds, false, 2, 2);  // Expect: no leading 0
- if (minutes > 0) display.showNumberDecEx(minutes, (0b01000000), false,2,0); 
-   
-   tone(BuzzerPin, 50, 50);
 
+      //display.showNumberDec(14, true, 2, 0); // Expect: __14
+      //delay(TEST_DELAY);
 
- 
+      display.setBrightness(0x0f);
+      if (minutes == 0)display.clear();
+      if (minutes > 0)display.showNumberDec(seconds, true, 2, 2); // Expect:  1:04
+      if (minutes == 0)display.showNumberDec(seconds, false, 2, 2); // Expect: no leading 0
+      if (minutes > 0) display.showNumberDecEx(minutes, (0b01000000), false, 2, 0);
+
+      tone(BuzzerPin, 50, 50);
 
 
 
 
-      
 
 
-      lcd.setCursor(4, 1);
+
+
+
+
+      //lcd.setCursor(4, 1);
       if (hours <= 9)
       {
-        lcd.print("0");
+        // lcd.print("0");
       }
-      lcd.print(hours);
-      lcd.print(":");
+      //lcd.print(hours);
+      //lcd.print(":");
       if (minutes <= 9)
       {
-        lcd.print("0");
+        //lcd.print("0");
       }
-      lcd.print(minutes);
-      lcd.print(":");
+      //lcd.print(minutes);
+      // lcd.print(":");
       if (seconds <= 9)
       {
-        lcd.print("0");
+        //lcd.print("0");
       }
-      lcd.print(seconds);
+      // lcd.print(seconds);
 
 
       if (digitalRead(Start) == LOW) {           // pauze countdown with start button
